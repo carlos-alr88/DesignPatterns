b2de3adb618acab62ecd30083fc1f216e59da0fd
Daniel <wingsonheaven@hotmail.com>
2022-11-23 14:09:09 -0600

    Ejemplo

--- design_patterns/src/com/daniel/designpatterns/memento/MementoMain.java
+++ design_patterns/src/com/daniel/designpatterns/memento/MementoMain.java
@@ -2,6 +2,6 @@ package design_patterns.src.com.daniel.designpatterns.memento;
 
 public class MementoMain {
     public static void main(String[] args) {
-        
+        System.out.println();
     }
 }
