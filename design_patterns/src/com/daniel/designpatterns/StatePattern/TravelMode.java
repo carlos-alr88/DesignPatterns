333abe2bdd41483ca643bd3a64e103729ddfd772
Daniel <wingsonheaven@hotmail.com>
2022-12-08 14:25:20 -0600

    StatePattern

---
+++ design_patterns/src/com/daniel/designpatterns/StatePattern/TravelMode.java
@@ -0,0 +1,13 @@
+package design_patterns.src.com.daniel.designpatterns.StatePattern;
+
+public interface TravelMode {
+    
+    Object getETA();
+    Object getDirection();
+    /*DRIVING,
+    *BICYCLING,
+    *TRANSIT,
+    *WALKING
+    */
+    
+}
